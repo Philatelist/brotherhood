8b3894a59b37bee32cba9194536e350000636ce0
Alexander Usov <a.usoff@gmail.com>
2017-06-13 19:46:22 +0300

    Initial commit

---
+++ src/main/java/ua/com/hellomoto/brotherhood/BrotherhoodApplication.java
@@ -0,0 +1,12 @@
+package ua.com.hellomoto.brotherhood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrotherhoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrotherhoodApplication.class, args);
+	}
+}
---
+++ src/main/java/ua/com/hellomoto/brotherhood/controller/WelcomeController.java
@@ -0,0 +1,21 @@
+package ua.com.hellomoto.brotherhood.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@EnableAutoConfiguration
+public class WelcomeController {
+
+    //for test external property file
+//    @Value("${level.advanced.score}")
+//    private double ADVANCED_SCORE;
+
+    @RequestMapping("/app/")
+    public String index() {
+        return "Welcome to Brotherhood Application! version 0.0.1";
+//                + ADVANCED_SCORE;
+
+    }
+
+}
